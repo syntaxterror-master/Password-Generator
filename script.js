6223f59fbd3d0a66bef3dc488545b752d54b5ec5
syntaxerror-master <bracketbender@gmail.com>
2025-02-23 19:34:59 +0200

    Added function to copy password by clicking on it and can set your own length

--- script.js
+++ script.js
@@ -5,7 +5,9 @@ const characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O"
   ,"?",
 "/"];
 
-let length = 15
+let length = 0
+let maxLength = 23
+let minLength = 6
 
 
 let maincontainerEl = document.getElementById("main-container")
@@ -17,37 +19,94 @@ let toggleOn = document.getElementById("toggle-on")
 let toggleOff = document.getElementById("toggle-off")
 let firstPassword = document.getElementById("password-first")
 let secondPassword = document.getElementById("password-second")
+let lengthEl = document.getElementById("length-el")
 
 
 function generatePassword() {
    randomPasswordOne()
    randomPasswordTwo()
+   lengthEl.value = ""
+ }
+
+ function copyPasswordOne() {
+  firstPassword.select()
+  firstPassword.setSelectionRange(0, 99999)
+  navigator.clipboard.writeText(firstPassword.value)
+
+
+ }
+
+ function copyPasswordTwo() {
+  secondPassword.select()
+  secondPassword.setSelectionRange(0, 99999)
+  navigator.clipboard.writeText(secondPassword.value)
+
  }
  
+function setPasswordLength() {
+  length = lengthEl.value
+} 
 
 function randomPasswordOne() {
+  setPasswordLength() 
   let passwordOne = ""
-  for (let i = 0; i < length; i++) {
-    let randomChars = Math.floor(Math.random() * 90)
+
+  if (length < minLength) {
+    firstPassword.value = "Password is to short!"
+    setTimeout(() => {
+      firstPassword.value = ""
+    }, 2000)
+  } else if (length > maxLength) {
+    firstPassword.value = "Password is to long!"
+    setTimeout(() => {
+      firstPassword.value = ""
+    }, 2000)
+  } else {
+    for (let i = 0; i < length; i++) {
+      let randomChars = Math.floor(Math.random() * characters.length)
     passwordOne += characters[randomChars]
-}
-firstPassword.innerText = passwordOne
+  }
+  firstPassword.value = passwordOne
+  firstPassword.addEventListener("click", (() => {
+    copyPasswordOne()
+    firstPassword.value = "Password Copied!"
+    setTimeout(() => {
+      firstPassword.value = passwordOne
+    }, 2000)
+  }))
+  }
  }
-
-
-
 
 
  function randomPasswordTwo() {
+  setPasswordLength() 
   let passwordTwo = ""
-  for (let i = 0; i < length; i++) {
-    let randomChars = Math.floor(Math.random() * 90)
-  passwordTwo += characters[randomChars]
-}
-  secondPassword.innerText = passwordTwo
+
+  if (length < minLength) {
+    secondPassword.value = "Password is to short!"
+    setTimeout(() => {
+      secondPassword.value = ""
+    }, 2000)
+  } else if (length > maxLength) {
+    secondPassword.value = "Password is to long!"
+    setTimeout(() => {
+      secondPassword.value = ""
+    }, 2000)
+  } else {
+    for (let i = 0; i < length; i++) {
+      let randomChars = Math.floor(Math.random() * characters.length)
+    passwordTwo += characters[randomChars]
+  }
+  secondPassword.value = passwordTwo
+  secondPassword.addEventListener("click", (() => {
+    copyPasswordTwo()
+    secondPassword.value = "Password Copied!"
+    setTimeout(() => {
+      secondPassword.value = passwordTwo
+    }, 2000)
+  }))
+  }
  }
-
-
 
 
 function darkTheme() {
